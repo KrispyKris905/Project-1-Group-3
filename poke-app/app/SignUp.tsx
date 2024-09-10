123c02689783d9ec695cc8ad33ddcfe1d1f89d32
Ashley Jaramillo <132930723+xeahh@users.noreply.github.com>
2024-09-10 14:13:36 -0700

    fixed sign up and login pages

--- poke-app/app/SignUp.tsx
+++ poke-app/app/SignUp.tsx
@@ -4,7 +4,8 @@ import { useNavigation } from '@react-navigation/native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-export default function HomeScreen() {
+
+export default function SignupScreen() {
     return (
         <ThemedView style={styles.titleContainer}>
           <ThemedText type="title">Welcome!</ThemedText>
