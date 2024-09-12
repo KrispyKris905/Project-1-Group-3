85e7c82563fba838702ed763427c2618c914d97f
Ashley Jaramillo <132930723+xeahh@users.noreply.github.com>
2024-09-11 18:44:07 -0700

    Login button goes to menu page Sign out button goes to landing page

--- poke-app/app/SignUp.tsx
+++ poke-app/app/SignUp.tsx
@@ -5,7 +5,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+export default function SignupScreen() {
     return (
         <ThemedView style={styles.titleContainer}>
           <ThemedText type="title">Welcome!</ThemedText>
